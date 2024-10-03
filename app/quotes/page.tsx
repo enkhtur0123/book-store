ea6ff1ab2a2ea1383ce84e5b8ed1c4287d31aa4d
Enkhtur <32415533+enkhtur0123@users.noreply.github.com>
2024-10-03 11:00:19 +0800

    initial commit

---
+++ app/quotes/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Quotes } from "../components/quotes/Quotes";
+
+export default function QuotesPage() {
+  return (
+    <>
+      <h1>Quotes page</h1>
+      <p>This page is intended to showcase RTK Query.</p>
+      <Quotes />
+    </>
+  );
+}
